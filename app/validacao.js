c75269f885a3bff6c00a77d6e2621c86d0042731
Lucas Peixoto <120954587+lucaspeixotochv@users.noreply.github.com>
2023-03-05 17:21:11 -0300

    Envio dos arquivos

---
+++ app/validacao.js
@@ -0,0 +1,35 @@
+function verificaSeOChutePossuiValorValido(chute){
+    const numero = +chute
+
+    if(chuteForInvalido(numero)){
+        elementoChute.innerHTML += '<div>Valor inválido!</div>' 
+        return
+    }
+
+    if(numeroForMaiorOuMenorQueOPermitido(numero)){
+        elementoChute.innerHTML += `Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}`
+        return
+    }
+
+    if(numero === numeroSecreto){
+        document.body.innerHTML = `
+        <h2>Você acertou!</h2>
+        <h3>O número secreto era ${numeroSecreto}</h3>
+        <button class="jogar" onclick="location.reload()">Jogar novamente</button>`
+
+    } else if (numero > numeroSecreto){
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>`
+    } else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>`
+    }
+}
+
+function chuteForInvalido(numero){
+    return Number.isNaN(numero)
+}
+
+function numeroForMaiorOuMenorQueOPermitido(numero){
+    return numero > maiorValor || numero < menorValor
+}
